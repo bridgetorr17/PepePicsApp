0730401d2bd236e2d3fc3328625f489fbf9f392a
bridgetorr17 <bridgetorr1902@gmail.com>
2025-05-09 16:47:17 -0400

    render images from GET

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 try{
-    fetch('http://localhost:3000/api/getFelines')
+    fetch('https://pepe-pics-backend.vercel.app/api/getFelines')
     .then(response => response.json())
     .then(data => {
         //add concert data already existing in the DB
@@ -13,5 +13,18 @@ catch(error){
 }
 
 function addElement(cat){
-    console.log(cat);
+    let catFeed = document.getElementById('catFeedList');
+    let li = document.createElement('li');
+    let img = document.createElement('img');
+    let name = document.createElement('span');
+    let caption = document.createElement('span');
+
+    img.setAttribute('src', cat.url);
+    name.innerHTML = cat.name;
+    caption.innerHTML = cat.caption;
+
+    li.appendChild(img);
+    li.appendChild(name);
+    li.appendChild(caption);
+    catFeed.appendChild(li);
 }
