68004b035c1e79d3cf7b55dc7bbba59f68a6af63
bridgetorr17 <bridgetorr1902@gmail.com>
2025-05-10 17:19:42 -0400

    styling front page

--- main.js
+++ main.js
@@ -1,16 +1,34 @@
+getFelines();
+
+//popup window logic and event listeners
+const popupButton = document.querySelector('#postPopup');
+const popupClose = document.querySelector('#popupClose');
+
+popupButton.addEventListener('click', _ => {
+    document.getElementById("overlay").style.display = "block";
+    document.getElementById("popupContainer").style.display = "block";
+});
+
+popupClose.addEventListener('click', _ => {
+    document.getElementById("overlay").style.display = "none";
+    document.getElementById("popupContainer").style.display = "none";
+})
+
 //get cat feed from server
-try{
-    fetch('https://pepe-pics-backend.vercel.app/api/getFelines')
-    .then(response => response.json())
-    .then(data => {
-        //add concert data already existing in the DB
-        data.forEach((cat) => {
-            addElement(cat);
-        });
-    })
-}
-catch(error){
-    console.log(error);
+function getFelines(){
+    try{
+        fetch('https://pepe-pics-backend.vercel.app/api/getFelines')
+        .then(response => response.json())
+        .then(data => {
+            //add concert data already existing in the DB
+            data.forEach((cat) => {
+                addElement(cat);
+            });
+        })
+    }
+    catch(error){
+        console.log(error);
+    }
 }
 
 //POST a cat photo with JS(handled here to prevent showing photo object from server response)
@@ -31,6 +49,10 @@ document.getElementById('uploadCat').addEventListener('submit', async function(e
     catch(error){
         console.log(error);
     }
+
+    //reload the page with the updated post
+    document.getElementById('catFeedList').innerHTML = "";
+    getFelines();
 })
 
 function addElement(cat){
@@ -39,13 +61,19 @@ function addElement(cat){
     let img = document.createElement('img');
     let name = document.createElement('span');
     let caption = document.createElement('span');
+    let section = document.createElement('section');
 
     img.setAttribute('src', cat.url);
+    img.setAttribute('class', 'catImage')
+    name.setAttribute('class', 'name');
+    caption.setAttribute('class', 'caption');
+    section.setAttribute('class', 'catContent darkPurple');
     name.innerHTML = cat.name;
     caption.innerHTML = cat.caption;
 
     li.appendChild(img);
-    li.appendChild(name);
-    li.appendChild(caption);
+    li.appendChild(section);
+    section.appendChild(name);
+    section.appendChild(caption);
     catFeed.appendChild(li);
 }
